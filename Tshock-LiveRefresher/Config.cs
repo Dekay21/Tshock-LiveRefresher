235c41ef5ce02ae20eb0993d463ab3f634efa056
Dekay <43781572+Dekay21@users.noreply.github.com>
2022-12-10 14:31:57 +0100

    Add project files.

---
+++ Tshock-LiveRefresher/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace Tshock_LiveRefresher
+{
+    internal class Config
+    {
+        public string TshockPath = "./";
+        public string ExecutableName = "Tshock.Server.exe";
+        public int RefreshInterval = 3000;
+        public string[] Plugins = Array.Empty<string>();
+        public string ServerParameters = "";
+    }
+}
---
+++ Tshock-LiveRefresher/Program.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace Tshock_LiveRefresher
+{
+    internal class Program
+    {
+        static readonly string ConfigName = "tshock-liverefresher.json";
+        static readonly string ServerPluginsName = "ServerPlugins";
+        static Config Config;
+        static Process Process;
+
+        static void Main(string[] args)
+        {
+            if (!File.Exists(ConfigName))
+            {
+                File.WriteAllText(ConfigName, JsonConvert.SerializeObject(new Config(), Formatting.Indented));
+                Console.WriteLine("Could not find config. Please fill the " + ConfigName + " next to the executable");
+                Environment.Exit(1);
+            }
+
+            Config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(ConfigName));
+            if (Config == null)
+            {
+                Console.WriteLine("Invalid config. Please check " + ConfigName);
+                Environment.Exit(1);
+            }
+
+            while (true)
+            {
+                List<string> toCopy = new List<string>();
+                foreach (string path in Config.Plugins)
+                {
+                    if (CheckFile(path))
+                    {
+                        toCopy.Add(path);
+                    }
+
+                }
+
+                if (toCopy.Count > 0 || Process == null || Process.HasExited)
+                {
+                    RestartServer(toCopy);
+                }
+
+                Thread.Sleep(Config.RefreshInterval);
+            }
+        }
+
+        static bool CheckFile(string pluginPath)
+        {
+            string filename = Path.GetFileName(pluginPath);
+            string newLocation = Path.Combine(Config.TshockPath, ServerPluginsName, filename);
+            if (!File.Exists(newLocation) || File.GetLastWriteTimeUtc(pluginPath) > File.GetLastWriteTimeUtc(newLocation))
+            {
+                Console.WriteLine($"{filename} has changed.");
+                return true;
+            }
+            return false;
+        }
+
+        static void RestartServer(List<string> filesToCopy)
+        {
+            Console.WriteLine("Restarting server.");
+            if (Process != null && !Process.HasExited)
+            {
+                Process.Kill();
+            }
+            foreach (string file in filesToCopy)
+            {
+                string newLocation = Path.Combine(Config.TshockPath, ServerPluginsName, Path.GetFileName(file));
+                File.Copy(file, newLocation, true);
+            }
+            var startInfo = new ProcessStartInfo(Path.Combine(Config.TshockPath, Config.ExecutableName), Config.ServerParameters);
+            startInfo.WorkingDirectory = Config.TshockPath;
+            Process = Process.Start(startInfo);
+        }
+    }
+}
